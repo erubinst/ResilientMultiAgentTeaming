56c6d7e8513b10f59d5755a537a3b15b6b6033cd
Esme Rubinstein <erubinst@andrew.cmu.edu>
2026-01-15 16:45:22 -0500

    updated config for new computer

--- cp_model/config.py
+++ cp_model/config.py
@@ -3,7 +3,7 @@
 # H should ideally be in the request file
 EPOCH_DATE = "2025-05-19T00:00"
 H = 10080
-DATA_PATH = "/Users/esmerubinstein/Desktop/ICLL/ResilientMultiAgentTeaming/scenarios/"
+DATA_PATH = "/Users/erubinst/ICLL/ResilientMultiAgentTeaming/scenarios"
 SCENARIO = "piggybacking_week"
 CPLEX_PATH = "/Applications/CPLEX_Studio2211/cpoptimizer/bin/arm64_osx/cpoptimizer"
 URL = "localhost:9087"
